e8e68600eaf9eecc7f65d17e9704c30963ceebae
김민규 <hayarobys@gmail.com>
2017-06-27 10:41:21 +0900

    레이팅바 실습

--- Chap05_ToastEvent/app06_ratingbar/src/main/java/com/model/choong/chap05_toastevent/MainActivity.java
+++ Chap05_ToastEvent/app06_ratingbar/src/main/java/com/model/choong/chap05_toastevent/MainActivity.java
@@ -1,13 +1,27 @@
 package com.model.choong.chap05_toastevent;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
+	RatingBar rbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-}
+
+		rbar = (RatingBar)findViewById(R.id.ratingBar);
+		rbar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener(){
+			@Override
+			public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser){
+				Toast.makeText(	getApplicationContext(),
+						"RatingBar Point: " + rating,
+						Toast.LENGTH_SHORT
+				).show();
+			}	// onRatingChanger()
+		});
+	}	// onCreate(Bundle)
+}	// class MainActivity{}
