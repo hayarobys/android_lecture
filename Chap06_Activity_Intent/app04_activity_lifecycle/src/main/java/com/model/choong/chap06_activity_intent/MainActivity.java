6b0ceb16e8db19e62f5f8bb08c29892bc44fde44
김민규 <hayarobys@gmail.com>
2017-06-27 15:52:56 +0900

    라이프사이클 테스트 코드 추가

--- Chap06_Activity_Intent/app04_activity_lifecycle/src/main/java/com/model/choong/chap06_activity_intent/MainActivity.java
+++ Chap06_Activity_Intent/app04_activity_lifecycle/src/main/java/com/model/choong/chap06_activity_intent/MainActivity.java
@@ -1,7 +1,8 @@
 package com.model.choong.chap06_activity_intent;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity{
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Log.i("MyLOG", "onCreate() 호출");
+	}
+
+	@Override
+	protected void onStart(){
+		super.onStart();
+		Log.i("MyLOG", "onStart() 호출");
+	}	// Log.i() 의 i는 inform의 약자.
+
+
+
+	@Override
+	protected void onResume(){
+		super.onResume();
+		Log.i("MyLOG", "onResume() 호출");
 	}
 }
