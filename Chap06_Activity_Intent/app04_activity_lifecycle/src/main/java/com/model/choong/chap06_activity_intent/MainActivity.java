054f0c2eec8b8fb06ad643e1688cf8f0ecca65e8
김민규 <hayarobys@gmail.com>
2017-06-27 16:48:16 +0900

    종료시 라이플 사이클 실험

--- Chap06_Activity_Intent/app04_activity_lifecycle/src/main/java/com/model/choong/chap06_activity_intent/MainActivity.java
+++ Chap06_Activity_Intent/app04_activity_lifecycle/src/main/java/com/model/choong/chap06_activity_intent/MainActivity.java
@@ -3,6 +3,7 @@ package com.model.choong.chap06_activity_intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -25,5 +26,30 @@ public class MainActivity extends AppCompatActivity{
 	protected void onResume(){
 		super.onResume();
 		Log.i("MyLOG", "onResume() 호출");
+	}
+
+	public void btnClickFinish(View v){
+		finish();
+	}
+
+	@Override
+	protected void onPause(){
+		/*화면 전환 직전, 터치 이벤트를 잠깐 중지시키는 메소드?*/
+		super.onPause();
+		Log.i("MyLOG", "onPause() 호출");
+	}
+
+	@Override
+	protected void onStop(){
+		/*화면 전환이 이루어진 직후 호출되는 메소드*/
+		super.onStop();
+		Log.i("MyLOG", "onStop() 호출");
+	}
+
+	@Override
+	protected void onDestroy(){
+		/*모든 작업이 끝난후 메모리에서 내리는 메소드*/
+		super.onDestroy();
+		Log.i("MyLOG", "onDestroy() 호출");
 	}
 }
