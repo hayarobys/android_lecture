051ebc0544218aa65d199f26c01725e8c826073d
김민규 <hayarobys@gmail.com>
2017-06-28 15:56:34 +0900

    안드로이드 단위에 대한 주석 추가

--- Chap07_AdapterView/app/src/main/java/com/model/choong/chap07_adapterview/MainActivity.java
+++ Chap07_AdapterView/app/src/main/java/com/model/choong/chap07_adapterview/MainActivity.java
@@ -11,3 +11,22 @@ public class MainActivity extends AppCompatActivity{
 		setContentView(R.layout.activity_main);
 	}
 }
+
+
+/*
+* dip = density independency pixel(밀도 독립 픽셀?)
+*
+* mdpi - 320px - 1inch - 2.54cm
+* 1dp = 1pixel
+*
+* hdpi(mdpi의 1.5배) = 480px
+* 1dp = 1.5pixel
+*
+* xhdpi - 640px
+* 1dp = 2pixel
+*
+* xxhdpi - 960px
+* 1dp = 3pixel
+*
+* 16dp = 16sp (글자는 대각선 길이로 계산한다. sp는 이렇게 16sp일때의 대각선 길이에서 배율을 곱해가며 글자 크기를 계산한다.)
+* */
