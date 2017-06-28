ef743ac8aa2b8bbbed13701907fe6167575f4db3
김민규 <hayarobys@gmail.com>
2017-06-28 09:31:10 +0900

    버튼과 sub_액티비티 추가

--- Chap06_Activity_Intent/app05_explicit_intent/src/main/java/com/model/choong/chap06_activity_intent/MainActivity.java
+++ Chap06_Activity_Intent/app05_explicit_intent/src/main/java/com/model/choong/chap06_activity_intent/MainActivity.java
@@ -1,13 +1,22 @@
 package com.model.choong.chap06_activity_intent;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity{
+	EditText et;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		et = (EditText)findViewById(R.id.editText);
+	}
+
+	public void btnClick(View v){
+
 	}
 }
