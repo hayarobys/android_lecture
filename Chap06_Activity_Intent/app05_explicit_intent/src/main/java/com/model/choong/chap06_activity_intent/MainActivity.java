8b2d0dc4384899a3e7fc887599a7b46b37be5bd3
김민규 <hayarobys@gmail.com>
2017-06-28 09:55:10 +0900

    액티비티간 데이터 전달

--- Chap06_Activity_Intent/app05_explicit_intent/src/main/java/com/model/choong/chap06_activity_intent/MainActivity.java
+++ Chap06_Activity_Intent/app05_explicit_intent/src/main/java/com/model/choong/chap06_activity_intent/MainActivity.java
@@ -1,5 +1,6 @@
 package com.model.choong.chap06_activity_intent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,6 +18,12 @@ public class MainActivity extends AppCompatActivity{
 	}
 
 	public void btnClick(View v){
+		String str = et.getText().toString();
+		Intent intent = new Intent(MainActivity.this, SubActivity.class);
+		intent.putExtra("key", str);	// putExta() - 다른 액티비티로 데이터를 전달하는 메소드
+		// map처럼 key, value를 이용한다.
+
+		startActivity(intent);
 
 	}
 }
--- Chap06_Activity_Intent/app05_explicit_intent/src/main/java/com/model/choong/chap06_activity_intent/SubActivity.java
+++ Chap06_Activity_Intent/app05_explicit_intent/src/main/java/com/model/choong/chap06_activity_intent/SubActivity.java
@@ -1,7 +1,8 @@
 package com.model.choong.chap06_activity_intent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SubActivity extends AppCompatActivity{
 
@@ -9,5 +10,11 @@ public class SubActivity extends AppCompatActivity{
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sub);
-	}
-}
+
+		Intent intent = getIntent();	// 이 액티비티를 띄우면서 전달된 intent가 있다면 반환.
+		String name = intent.getExtras().getString("key");
+				// getExtras() - Intent에 담긴 데이터를 꺼내는 메소드.
+				// Object형식으로 반환되기에 getString()으로 문자열 변환.
+
+	}	// onCreate()
+}	// class SubActivity{}
