9528d577238d5578f980134bc2631d512e6a42e5
김민규 <hayarobys@gmail.com>
2017-06-30 10:35:19 +0900

    레이아웃, DB정보를 추가한다

---
+++ Chap08_DB/app/src/main/java/com/model/choong/chap08_db/StudentsDBManager.java
@@ -0,0 +1,14 @@
+package com.model.choong.chap08_db;
+
+/**
+ * Created by pc on 2017-06-30.
+ */
+
+public class StudentsDBManager{
+
+	// DB명, 테이블명, DB 버전 정보를 정의한다.
+	static final String DB_STUDENTS = "Students.db";
+	static final String TABLE_STUDENTS = "Students";
+	static final int DB_VERSION = 1;
+
+}
