b9f88430d6a846adc61ccd09ae0277232ffcbe1e
김민규 <hayarobys@gmail.com>
2017-07-05 09:09:12 +0900

    안드로이드 마시멜로 이상~ 안드로이드 스튜디오 2.4미만에 있는 버그로인해 Chap11미디어를 건너뛰고 Chap12를 추가한다

--- Chap10_ServiceBroadcast/app03_bcReceiverNotification/src/main/java/com/model/choong/chap10_servicebroadcast/MainActivity.java
+++ Chap10_ServiceBroadcast/app03_bcReceiverNotification/src/main/java/com/model/choong/chap10_servicebroadcast/MainActivity.java
@@ -1,11 +1,15 @@
 package com.model.choong.chap10_servicebroadcast;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,13 +25,13 @@ public class MainActivity extends AppCompatActivity{
 		btn.setOnClickListener(new View.OnClickListener(){
 			@Override
 			public void onClick(View v){
-
+				handler.sendEmptyMessageDelayed(0, 5000);	// 5초 뒤에 이벤트 발생
 			}
 		});
 	}
 
 	Handler handler = new Handler(){
-		// 이벤트가 발생하면 안드로이드는 handleMessage를 호출?
+		// BroadcastReceiver는 이벤트를 수신하면 handleMessage()를 호출함.
 		@Override
 		public void handleMessage(Message msg){
 			super.handleMessage(msg);
@@ -45,6 +49,25 @@ public class MainActivity extends AppCompatActivity{
 		// 인텐트 전달자 객체에 인텐트를 포장한다.
 		// 여러개 전송할땐 getActivities(), 단일전송엔 getActivity()
 		PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-		
-	}
+
+		// Notification 객체 생성(알림 메세지 생성)
+		Notification notification = new NotificationCompat.Builder(this)
+				.setContentIntent(pendingIntent)
+				.setSmallIcon(android.R.drawable.ic_lock_idle_alarm)
+				.setContentTitle("알림 제목")
+				.setContentText("알림 내용입니다...")
+				.setTicker("알림 메세지 도착")
+				.setAutoCancel(true)
+				.build();
+		// setTicker()는 뭐지? 액션바 상단에 보여지는 글씨라는데 아이콘만 달랑 보이네.
+
+		// 2. NotificationManager 객체 가져오기
+		NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+		// 3. 알림 띄우기(알림 아이디, Notification 객체)
+		notificationManager.notify(999, notification);
+
+		// 4. 원하는 시점에 취소 할 거라면, 알림의 아이디값을 알고 있으면 된다.
+		notificationManager.cancel(999);
+	} // showNotification()
 } // class MainActivity{}
